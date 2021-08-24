156228a3374c915d60d508e02943bccbc9a27168
Marius Larsen <mariubla@ntnu.no>
2021-08-24 18:41:56 +0200

    refactor and validation with Yup
    * import and export default all form component from common forms-folder
    * validation of email and password field in login form
        * including reusable formfield-component with errormessage component

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Card from "./app/components/Card/Card";
+import { Switch, Text, TextInput, View } from "react-native";
+import colors from "./app/config/colors";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import MyAccountScreen from "./app/screens/MyAccountScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <LoginScreen />;
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,43 +1,44 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
-import { Formik } from "formik";
+import * as Yup from "yup";
 
-import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
-import AppButton from "../components/AppButton";
+import { AppFormField, SubmitButton, AppForm } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 export default function LoginScreen(props) {
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-      <Formik
+      <AppForm
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => (
-          <>
-            <AppTextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              icon="email"
-              placeholder="Email"
-              keyboardType="email-address"
-              onChangeText={handleChange("email")}
-              textContentType="emailAddress"
-            />
-            <AppTextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              icon="lock"
-              placeholder="Password"
-              textContentType="password"
-              onChangeText={handleChange("password")}
-              secureTextEntry
-            />
-            <AppButton text="Login" onPress={handleSubmit} />
-          </>
-        )}
-      </Formik>
+        <AppFormField
+          name="email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          placeholder="Email"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          name="password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
     </Screen>
   );
 }
